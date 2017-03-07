b4eefcd3fb1733fc362cb30fab817ddcc8d1108e
David Hagen <david@drhagen.com>
2017-03-07 08:17:26 -0500

    Placate flake8.

--- sheet/tests/test_dice.py
+++ sheet/tests/test_dice.py
@@ -1,5 +1,5 @@
-from unittest import TestCase
 from math import sqrt
+from unittest import TestCase
 
 from sheet.models.dice import DiceFormula
 
@@ -8,7 +8,8 @@ class StatisticsTestCase(TestCase):
     def test_statistics(self):
         six = DiceFormula.from_str('d6')
         self.assertAlmostEqual(six.mean(), 3.5)
-        self.assertAlmostEqual(six.std(), sqrt(105/36))  # https://stats.stackexchange.com/questions/198025/
+        # Variance from https://stats.stackexchange.com/questions/198025/
+        self.assertAlmostEqual(six.std(), sqrt(105/36))
 
         six_plus_one = DiceFormula.from_str('d6 + 1')
         self.assertAlmostEqual(six_plus_one.mean(), 4.5)
